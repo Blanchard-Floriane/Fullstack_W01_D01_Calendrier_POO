64e3292840a9148006eae59c9fed442cd9dabbcd
LaSnipeuse <floriane.blanchard.pro@gmail.com>
2023-10-17 08:45:11 +0200

    add modifications from corrections

--- lib/event.rb
+++ lib/event.rb
@@ -1,50 +1,42 @@
 class Event
   attr_accessor :start_date, :duration, :title, :attendees
   #Ainsi notre classe Event comprend 4 attributs (ou variables d'instance qu'on écrit avec @....)
-  @@all_events = [] #variable de classe
 
-  def initialize(start_date_to_save, duration_to_save, title_to_save, attendees_to_save)
+  def initialize(start_date, duration, title, attendees)
     #Pas plus de 4 paramètres par méthode.
-    @start_date = Time.parse(start_date_to_save)
-    @duration = duration_to_save.to_i
-    @title = title_to_save.to_s
-    @attendees = attendees_to_save
-    @@all_events.push(self)
+    @start_date = Time.parse(start_date)
+    @duration = duration.to_i
+    @title = title.to_s
+    @attendees = attendees
   end
 
   def postpone_24h
-    @start_date = @start_date + 24 * 60 * 60
+    @start_date += 24 * 60 * 60
     #en ajoutant + 86 400 (soit le nb de secondes dans une heure) cela décale la date de début de l'événement de 24h 
   end
 
   def end_date
-    @start_date + @duration
+    return @start_date + @duration*60
   end
 
   def is_past? #la convention veut que si la méthode renvoie un bouléen on ajoute un "?"
-    @start_date < Time.now
+    return @start_date < Time.now
   end
 
-  def is_future? #on aurait aussi pu obtenir l'inverse avec "!". Exemple dans PRY, si je tape : !my_event.is_past? 
-    @start_date > Time.now
+  def is_future?
+    !self.is_past?
   end
 
   def is_soon?
-    @start_date < (Time.now + 1800) #1800 secondes ce qui correspond à 30 min
+    return (is_past?) && (@start_date > (Time.now - 30*60) #1800 secondes ce qui correspond à 30 min
   end
 
   def to_s
-    event_info = ">Titre : #{@title}\n"
-    event_info += ">Date de début : #{@start_date}\n"
-    event_info += ">Durée : #{@duration / 60} minutes\n"
-    event_info += ">Invités : " + @attendees.join(", ")
-    return event_info
+    puts ">Titre : #{@title}\n"
+    puts ">Date de début : #{@start_date}\n"
+    puts ">Durée : #{@duration / 60} minutes\n"
+    puts ">Invités : " + @attendees.join(", ")
   end
-
-  def self.all
-    return @@all_events
-  end
-
 end
 
 #un event a un ou pls invités, ces invités sont définis par leur email tel que ["truc@machin.com", "bidule@chose.fr"]
--- lib/user.rb
+++ lib/user.rb
@@ -2,11 +2,15 @@ class User
   attr_accessor :email, :age #2 instances de ma class User
   @@all_users = [] #variable de classe pour initialiser l'array
 
-  def initialize(email_to_save, age_to_save)
+  def initialize(email, age)
     #Dès la création d'un `User`, on veut définir son e-mail et son âge.
-    @email = email_to_save
-    @age = age_to_save
-    @@all_users.push(self) # Ajoutez l'instance actuelle à @@all_users, pas les valeurs individuelles
+    @email = email
+    @age = age
+    @@all_users << self # Ajoutez l'instance actuelle à @@all_users, pas les valeurs individuelles
+  end
+  
+  def self.all
+    return @@all_users
   end
 
   def self.find_by_email(email)
@@ -17,10 +21,5 @@ class User
       puts "Aucun utilisateur trouvé avec cet e-mail"
     end
   end
-
-  def self.all
-    return @@all_users
-  end
-
 end
 
